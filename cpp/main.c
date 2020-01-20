7424f1f2209ab47f59c5476975aaa727b6ccff16
gdepuydt1983 <geert.depuydt@ontoforce.com>
2020-01-20 20:49:06 +0100

    c version

--- cpp/main.c
+++ cpp/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUF_SIZE 256 * 1024
+#define MAX_RESULTS 256
 
 struct Mistake {
     // Human readable description of the mistake
     char *message;
+
+    // Points to where the mistake is in the text
+    char *location;
 } Mistake;
 
 struct CheckResult {
@@ -28,8 +33,16 @@ size_t read_file(char *path, char *buf) {
 void check_mistakes(char *buf, size_t len, struct CheckResult *result) {
     for (size_t i = 0; i < len; i++) {
         if (buf[i] == ',') {
-            struct Mistake *m = malloc(sizeof(Mistake));
-            m->message = "commas are forbidden!\n";
+            struct Mistake *m = malloc(sizeof(struct Mistake));
+            size_t location_len = len - i;
+            if (location_len > 12) {
+                location_len = 12;
+            }
+                     
+            m->location = malloc(location_len + 1);
+            memcpy(m->location, &buf[i], location_len);
+            m->location[location_len] = '\0';
+            m->message = "commas are forbidden";
             result->mistake = m;
             break;
         }
@@ -44,19 +57,50 @@ struct CheckResult* check(char *path, char *buf) {
     // read file in buf and return the length of the file 
     size_t len = read_file(path, buf);
 
-    // check for mistakes, and wite in result
+    // check for mistakes, and write in result
     check_mistakes(buf, len, result);
     
     return result;
 }
 
+void report(struct CheckResult *result) {
+    printf("\n");
+    printf("~ %s ~\n", result->path);
+    if(result->mistake == NULL) {
+        printf("No mistakes were found!\n");
+    }
+    else {
+        // TODO (Geert): print mistake text
+        printf(
+            "mistake: %s: '%.12s'\n",
+            result->mistake->message,
+            result->mistake->location
+        );
+    }
+}
+
 int main() {
     char buf[BUF_SIZE];
 
-    struct CheckResult *result = check("sample.txt", buf);
-    if(result->mistake != NULL) {
-        printf("Commas are forbidden!\n");
-        return 0;
+    struct CheckResult *bad_results[MAX_RESULTS];
+    int num_results = 0;
+    
+    char *paths[] = {"sample3.txt", "sample2.txt", "sample.txt", NULL};
+
+    for (int i = 0; paths[i] != NULL; i++) {
+        char *path = paths[i];
+        struct CheckResult *result = check(path, buf);
+        bad_results[num_results++] = result;
+    }
+
+    for (int i = 0; i < num_results; i++) {
+        report(bad_results[i]);
+    }
+
+    for (int i = 0; i < num_results; i++) {
+        free(bad_results[i]->mistake->location);
+        free(bad_results[i]->mistake);
+        free(bad_results[i]);
     }
 
     return 0;
