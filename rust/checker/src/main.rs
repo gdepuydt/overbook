4bcf5e315253431b2d327a2987d0e00879b50fe1
gdepuydt1983 <geert.depuydt@ontoforce.com>
2020-01-27 14:24:22 +0100

    final version of checker

--- rust/checker/src/main.rs
+++ rust/checker/src/main.rs
@@ -1,42 +1,85 @@
-use std::fs::read_to_string;
-
-
-struct Mistake<'a> {
-    location: &'a str,
-}
-
 fn main() -> Result<(), std::io::Error> {
-    
-    let path = "sample.txt";
-    let mut s = String::with_capacity(256 * 1024);
-    let result = check(path, &mut s)?;
-    if let Some(mistake) = result {
-        println!("mistake: commas are forbidden: {:?}", mistake.location);
+    let paths = ["sample4.txt"];
+
+    // check all documents
+    let mut results = vec![];
+    for path in &paths {
+        let result = check(path)?;
+        results.push(result);
+    }
+
+    // report them all
+    for result in results {
+        report(result);
     }
 
     Ok(())
 }
 
-use std::io::Error as E;
-
-fn check<'a>(path: &str, s: &'a mut String) -> Result<Option<Mistake<'a>>, E> {
-    
-    use std::fs::File;
-    use std::io::Read;
-    
-    s.clear();
-    File::open(path)?.read_to_string(s)?;
-
-    println!("~ {} ~", path);
-
-    Ok(match s.find(",") {
-        Some(index) => {
-            let location = &s[index..];
-            Some(Mistake {location})
-        }
-        None => None,
-    })
-
+fn report(result: Option<Mistake>) {
+    if let Some(mistake) = result {
+        println!("{}", mistake);
+    }
 }
 
-// TODO(Geert): A little more structure
+struct Mistake {
+    path: &'static str,
+
+    text: String,
+    locations: Vec<usize>,
+}
+
+use std::io::Error as E;
+
+fn check(path: &'static str) -> Result<Option<Mistake>, E> {
+    let text = std::fs::read_to_string(path)?;
+
+    let locations: Vec<_> = text.match_indices(",").map(|(index, _)| index).collect();
+
+    Ok(if locations.is_empty() {
+        None
+    } else {
+        Some(Mistake {
+            path,
+            text,
+            locations,
+        })
+    })
+}
+
+use std::fmt;
+
+impl Mistake {
+    fn line_bounds(&self, index: usize) -> (usize, usize) {
+        let len = self.text.len();
+
+        let before = &self.text[..index];
+        let start = before.rfind("\n").map(|x| x + 1).unwrap_or(0);
+
+        let after = &self.text[index + 1..];
+        let end = after.find("\n").map(|x| x + index + 1).unwrap_or(len);
+
+        (start, end)
+    }
+}
+
+impl fmt::Display for Mistake {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for &location in &self.locations {
+            let (start, end) = self.line_bounds(location);
+            let line = &self.text[start..end];
+
+            let line_number = self.text[..start].matches("\n").count() + 1;
+            let comma_index = location - start;
+
+            write!(f, "{}: commas are forbidden:\n\n", self.path)?;
+
+            // print the line, with line number
+            write!(f, "{:>8} | {}\n", line_number, line)?;
+
+            // indicate where the comma is
+            write!(f, "{}^\n\n", " ".repeat(11 + comma_index))?;
+        }
+        Ok(())
+    }
+}
